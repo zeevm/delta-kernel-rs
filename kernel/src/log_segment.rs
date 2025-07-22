9233172d13ae5611ec4a0e5e42f7c5a0c6f4ac05
OussamaSaoudi <45303303+OussamaSaoudi@users.noreply.github.com>
2025-07-22 10:57:12 -0700

    feat: Add log segment constructor for timestamp to version conversion (#895)
    ## What changes are proposed in this pull request?
    
    This is a stacked PR. Please look at the _LAST_ commit of each PR for
    the files changed.
    * [LogSegment constructor for timestamp
    conversion](https://github.com/delta-io/delta-kernel-rs/pull/895)
    * [Commit to timestamp helper
    function](https://github.com/delta-io/delta-kernel-rs/pull/898)
    * [ICT search bounds
    checking](https://github.com/delta-io/delta-kernel-rs/pull/899)
    * [High level timestamp conversion
    APIs](https://github.com/delta-io/delta-kernel-rs/pull/900)
    
    
    This PR adds a LogSegment constructor that will be used in timestamp to
    version conversion. The function `LogSegment::for_timestamp_conversion`
    creates a LogSegment made entirely of commits up to an optional end
    version.
    
    `LogSegment::for_timestamp_conversion` goes back to the earliest
    possible commit while maintaining contiguity of the LogSegment.
    
    ## How was this change tested?
    This checks that we construct a log segment in the following cases:
    - A normal range of commits
    - A delta log that is missing a commit.
    - A delta log that only has a checkpoint.
    
    Co-authored-by: Oussama Saoudi <oussama.saoudi@databricks.com>

--- kernel/src/log_segment.rs
+++ kernel/src/log_segment.rs
@@ -2,6 +2,7 @@
 //! files.
 use std::collections::HashMap;
 use std::convert::identity;
+use std::num::NonZero;
 use std::sync::{Arc, LazyLock};
 
 use crate::actions::visitors::SidecarVisitor;
@@ -202,6 +203,61 @@ impl LogSegment {
             None, // TODO: use CRC files for table changes?
         );
         LogSegment::try_new(listed_files, log_root, end_version)
+    }
+
+    #[allow(unused)]
+    /// Constructs a [`LogSegment`] to be used for timestamp conversion. This [`LogSegment`] will
+    /// consist only of contiguous commit files up to `end_version` (inclusive). If present,
+    /// `limit` specifies the maximum length of the returned log segment. The log segment may be
+    /// shorter than `limit` if there are missing commits.
+    ///
+    // This lists all files starting from `end-limit` if `limit` is defined. For large tables,
+    // listing with a `limit` can be a significant speedup over listing _all_ the files in the log.
+    pub(crate) fn for_timestamp_conversion(
+        storage: &dyn StorageHandler,
+        log_root: Url,
+        end_version: Version,
+        limit: Option<NonZero<usize>>,
+    ) -> DeltaResult<Self> {
+        // Compute the version to start listing from.
+        let start_from = limit
+            .map(|limit| match NonZero::<Version>::try_from(limit) {
+                Ok(limit) => Ok(Version::saturating_sub(end_version, limit.get() - 1)),
+                _ => Err(Error::generic(format!(
+                    "Invalid limit {limit} when building log segment in timestamp conversion",
+                ))),
+            })
+            .transpose()?;
+
+        let mut contiguous_commits: Vec<ParsedLogPath> =
+            limit.map_or_else(Vec::new, |limit| Vec::with_capacity(limit.get()));
+
+        for commit_res in list_log_files(storage, &log_root, start_from, Some(end_version))? {
+            let commit = match commit_res {
+                Ok(file) if matches!(file.file_type, LogPathFileType::Commit) => file,
+                Ok(_) | Err(Error::InvalidLogPath(_)) => continue, // Ignore non-commit
+                Err(err) => return Err(err), // Listing errors are propagated to the caller
+            };
+
+            if contiguous_commits
+                .last()
+                .is_some_and(|prev_commit| prev_commit.version + 1 != commit.version)
+            {
+                // We found a gap, so throw away all earlier versions
+                contiguous_commits.clear();
+            }
+
+            contiguous_commits.push(commit);
+        }
+
+        let listed_files = ListedLogFiles {
+            ascending_commit_files: contiguous_commits,
+            ascending_compaction_files: vec![],
+            checkpoint_parts: vec![],
+            latest_crc_file: None,
+        };
+
+        LogSegment::try_new(listed_files, log_root, Some(end_version))
     }
 
     /// Read a stream of actions from this log segment. This returns an iterator of
--- kernel/src/log_segment/tests.rs
+++ kernel/src/log_segment/tests.rs
@@ -1817,3 +1817,175 @@ fn commits_since() {
     assert_eq!(log_segment.commits_since_checkpoint(), 10);
     assert_eq!(log_segment.commits_since_log_compaction_or_checkpoint(), 1);
 }
+
+#[test]
+fn for_timestamp_conversion_gets_commit_range() {
+    let (storage, log_root) = build_log_with_paths_and_checkpoint(
+        &[
+            delta_path_for_version(0, "json"),
+            delta_path_for_version(1, "json"),
+            delta_path_for_version(1, "checkpoint.parquet"),
+            delta_path_for_version(2, "json"),
+            delta_path_for_version(3, "json"),
+            delta_path_for_version(3, "checkpoint.parquet"),
+            delta_path_for_version(4, "json"),
+            delta_path_for_version(5, "json"),
+            delta_path_for_version(5, "checkpoint.parquet"),
+            delta_path_for_version(6, "json"),
+            delta_path_for_version(7, "json"),
+        ],
+        None,
+    );
+
+    let log_segment =
+        LogSegment::for_timestamp_conversion(storage.as_ref(), log_root.clone(), 7, None).unwrap();
+    let commit_files = log_segment.ascending_commit_files;
+    let checkpoint_parts = log_segment.checkpoint_parts;
+
+    assert!(checkpoint_parts.is_empty());
+
+    let versions = commit_files.iter().map(|x| x.version).collect_vec();
+    assert_eq!(vec![0, 1, 2, 3, 4, 5, 6, 7], versions);
+}
+
+#[test]
+fn for_timestamp_conversion_with_old_end_version() {
+    let (storage, log_root) = build_log_with_paths_and_checkpoint(
+        &[
+            delta_path_for_version(0, "json"),
+            delta_path_for_version(1, "json"),
+            delta_path_for_version(1, "checkpoint.parquet"),
+            delta_path_for_version(2, "json"),
+            delta_path_for_version(3, "json"),
+            delta_path_for_version(3, "checkpoint.parquet"),
+            delta_path_for_version(4, "json"),
+            delta_path_for_version(5, "json"),
+            delta_path_for_version(5, "checkpoint.parquet"),
+            delta_path_for_version(6, "json"),
+            delta_path_for_version(7, "json"),
+        ],
+        None,
+    );
+
+    let log_segment =
+        LogSegment::for_timestamp_conversion(storage.as_ref(), log_root.clone(), 5, None).unwrap();
+    let commit_files = log_segment.ascending_commit_files;
+    let checkpoint_parts = log_segment.checkpoint_parts;
+
+    assert!(checkpoint_parts.is_empty());
+
+    let versions = commit_files.iter().map(|x| x.version).collect_vec();
+    assert_eq!(vec![0, 1, 2, 3, 4, 5], versions);
+}
+
+#[test]
+fn for_timestamp_conversion_only_contiguous_ranges() {
+    let (storage, log_root) = build_log_with_paths_and_checkpoint(
+        &[
+            delta_path_for_version(0, "json"),
+            delta_path_for_version(1, "json"),
+            delta_path_for_version(1, "checkpoint.parquet"),
+            delta_path_for_version(2, "json"),
+            delta_path_for_version(3, "json"),
+            delta_path_for_version(3, "checkpoint.parquet"),
+            // version 4 is missing
+            delta_path_for_version(5, "json"),
+            delta_path_for_version(5, "checkpoint.parquet"),
+            delta_path_for_version(6, "json"),
+            delta_path_for_version(7, "json"),
+        ],
+        None,
+    );
+
+    let log_segment =
+        LogSegment::for_timestamp_conversion(storage.as_ref(), log_root.clone(), 7, None).unwrap();
+    let commit_files = log_segment.ascending_commit_files;
+    let checkpoint_parts = log_segment.checkpoint_parts;
+
+    assert!(checkpoint_parts.is_empty());
+
+    let versions = commit_files.iter().map(|x| x.version).collect_vec();
+    assert_eq!(vec![5, 6, 7], versions);
+}
+
+#[test]
+fn for_timestamp_conversion_with_limit() {
+    let (storage, log_root) = build_log_with_paths_and_checkpoint(
+        &[
+            delta_path_for_version(0, "json"),
+            delta_path_for_version(1, "json"),
+            delta_path_for_version(1, "checkpoint.parquet"),
+            delta_path_for_version(2, "json"),
+            delta_path_for_version(3, "json"),
+            delta_path_for_version(3, "checkpoint.parquet"),
+            delta_path_for_version(4, "json"),
+            delta_path_for_version(5, "json"),
+            delta_path_for_version(5, "checkpoint.parquet"),
+            delta_path_for_version(6, "json"),
+            delta_path_for_version(7, "json"),
+        ],
+        None,
+    );
+
+    let log_segment = LogSegment::for_timestamp_conversion(
+        storage.as_ref(),
+        log_root.clone(),
+        7,
+        Some(NonZero::new(3).unwrap()),
+    )
+    .unwrap();
+    let commit_files = log_segment.ascending_commit_files;
+    let checkpoint_parts = log_segment.checkpoint_parts;
+
+    assert!(checkpoint_parts.is_empty());
+
+    let versions = commit_files.iter().map(|x| x.version).collect_vec();
+    assert_eq!(vec![5, 6, 7], versions);
+}
+#[test]
+fn for_timestamp_conversion_with_large_limit() {
+    let (storage, log_root) = build_log_with_paths_and_checkpoint(
+        &[
+            delta_path_for_version(0, "json"),
+            delta_path_for_version(1, "json"),
+            delta_path_for_version(1, "checkpoint.parquet"),
+            delta_path_for_version(2, "json"),
+            delta_path_for_version(3, "json"),
+            delta_path_for_version(3, "checkpoint.parquet"),
+            delta_path_for_version(4, "json"),
+            delta_path_for_version(5, "json"),
+            delta_path_for_version(5, "checkpoint.parquet"),
+            delta_path_for_version(6, "json"),
+            delta_path_for_version(7, "json"),
+        ],
+        None,
+    );
+
+    let log_segment = LogSegment::for_timestamp_conversion(
+        storage.as_ref(),
+        log_root.clone(),
+        7,
+        Some(NonZero::new(20).unwrap()),
+    )
+    .unwrap();
+    let commit_files = log_segment.ascending_commit_files;
+    let checkpoint_parts = log_segment.checkpoint_parts;
+
+    assert!(checkpoint_parts.is_empty());
+
+    let versions = commit_files.iter().map(|x| x.version).collect_vec();
+    assert_eq!(vec![0, 1, 2, 3, 4, 5, 6, 7], versions);
+}
+
+#[test]
+fn for_timestamp_conversion_no_commit_files() {
+    let (storage, log_root) = build_log_with_paths_and_checkpoint(
+        &[delta_path_for_version(5, "checkpoint.parquet")],
+        None,
+    );
+
+    let res = LogSegment::for_timestamp_conversion(storage.as_ref(), log_root.clone(), 0, None);
+    assert!(res.is_err());
+    let msg = res.err().unwrap().to_string();
+    assert!(msg.contains("No files in log segment"))
+}
